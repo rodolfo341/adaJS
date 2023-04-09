4095b1bf4257d9215239c3a872848c316c5df77a
Rodolfo Aravena <rodolfo.aravena.espinosa@gmail.com>
2023-04-09 15:27:24 -0400

    .

--- 1.02_InstruccionesDeSalida/app.js
+++
@@ -1,5 +0,0 @@
-
-window.alert("este es un alert");
-document.write("<h1>este es un docuiment.write usando un h1");
-document.getElementById("un_ID").innerHTML = "Parrafo cambiado";
-console.log("Texto para la consola");
